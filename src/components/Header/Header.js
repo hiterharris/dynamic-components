ecf820dd81df92c1bb2f97f4386784f92cec32fe
Henry Harris IV <henryharrisiv@Henrys-MBP.lan>
2023-03-28 22:02:16 -0400

    editable components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
+import { useState, useEffect } from "react";
 import HeaderStyled from "./HeaderStyled";
+import { EditText, EditTextarea } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
 
 function Header(props) {
     const { text, style } = props;
+    const [textValue, setTextValue] = useState(text)
+    useEffect(() => {
+        console.log(textValue)
+    }, [textValue])
     return (
         <HeaderStyled style={style}>
             <h1>{text || 'Default Header'}</h1>
+            <EditText showEditButton name="header" value={textValue} onChange={e => setTextValue(e.target.value)} />
         </HeaderStyled>
     )
 }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,10 +1,29 @@
 import ImageStyled from './ImageStyled'
+import { useState, useEffect } from "react";
 
 function Image(props) {
     const { src, alt, style } = props;
+    const [selectedImage, setSelectedImage] = useState()
+
+    useEffect(() => {
+        console.log(selectedImage)
+    }, [selectedImage])
+
     return (
         <ImageStyled style={style}>
             <img src={src} alt={alt} style={style?.img} />
+            <div className="">
+                <input type="file" id="file-input" name="ImageStyle"/>
+                {/* <input
+                    type="file"
+                    name="myImage"
+                    onChange={(event) => {
+                    console.log(event.target.files[0]);
+                    setSelectedImage(event.target.files[0]);
+                    }}
+                /> */}
+            </div>
+
         </ImageStyled>
     )
 }
